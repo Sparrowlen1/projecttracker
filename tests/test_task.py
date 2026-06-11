59fb93995a6f4229a8a3be3e7d8cc4adee93e776
Daniel Muthui <dannymuthui118@gmail.com>
2026-06-11 12:03:27 +0300

    feat: testing for the task

---
+++ tests/test_task.py
@@ -0,0 +1,8 @@
+import unittest
+import sys
+import sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from models.task import Task
+
+class TestTask(unittest.TestCase):
+    
